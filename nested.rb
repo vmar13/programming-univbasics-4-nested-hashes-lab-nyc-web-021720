ce5938d030d1e45885a1f3a972b2f3e6e23a39c5
Vanessa Martinez <vmar76@gmail.com>
2020-02-12 17:12:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,11 +60,13 @@ programmer_hash["dennis_ritchies_language"]="C"
 end
 
 def adding_matz
+  
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
+
 programmer_hash[:yukihiro_matsumoto]=[:known_for=>"Ruby", :languages=>["LISP", "C"]]
 	programmer_hash = 
  		{
